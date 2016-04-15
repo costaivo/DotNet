a27df8a87e4e875006edaaf502164bdd722350cf
costaivo <costa_ivo@yahoo.com>
2016-04-15 15:34:44 +0530

    Added console messages

--- TfsUtlities/TfsUtlities/Program.cs
+++ TfsUtlities/TfsUtlities/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace TfsUtlities
+﻿using System;
+
+namespace TfsUtlities
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Application: TFS Console App Running..");
+            Console.WriteLine("Press Any Key to Exit");
+            Console.ReadLine();
         }
     }
 }
