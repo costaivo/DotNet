b7990ecafc87195365fae7e33b2b1984e4cb3240
costaivo <costa_ivo@yahoo.com>
2017-03-18 00:31:31 +0530

    Added BootStrap

--- MVC/TodoApp/2/Controllers/ToDoController.cs
+++ MVC/TodoApp/2/Controllers/ToDoController.cs
@@ -14,19 +14,7 @@ namespace TodoApp.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
 
         public ActionResult AddItem(HomeViewModel homeViewModel)
         {
